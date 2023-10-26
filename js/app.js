e14bb604c8c3291b281f95b81be7801a563d2b3d
lana-z <lana@levelupeconomy.com>
2023-10-25 19:34:57 -0700

    class 03 lab added About Me

--- js/app.js
+++ js/app.js
@@ -7,16 +7,18 @@ console.log("Hello")
 
 alert("Hello! This website is about me. Glad you stopped by. Here's some trivia about me. Let's see how you do.");
 
-//let workTJ = userAccept.substring(0, 1).toLowerCase();
-//same as let workTJ = workTJ.charAt(0);
+//let workSE = userAccept.substring(0, 1).toLowerCase();
+//same as let workSE = workSE.charAt(0);
 //also can use str.startsWith() method https://www.geeksforgeeks.org/javascript-string-startswith-method/
 
-let workTJ = prompt("Do I work at Trader Joe's?").toLowerCase();
+let userScore = 0;
+let workSE = prompt("Do I work with startups?").toLowerCase();
 
 
-if (workTJ === "no") {
+if (workSE === "yes") {
    //console.log(That's correct!);
     alert("That's correct!");
+    userScore++;
 } else {
     //console.log("Nope.");
     alert("Nope.");
@@ -27,6 +29,7 @@ let liveNe = prompt("Do I live in Nebraska?").toLowerCase();
 if (liveNe === "no") {
         //console.log("That's correct!");
     alert("That's correct!");
+    userScore++;
 } else {
     //console.log("Whoops, wrong answer. I used to though.);
     alert("Whoops, wrong answer. I used to though!");
@@ -37,6 +40,7 @@ let iHaveKids = prompt("Do I have kids?").toLowerCase();
 if (iHaveKids === "yes") {
     //console.log("That's correct!");
     alert("That's correct!");
+    userScore++;
 } else {
     //console.log("Shoot. Actually I have two teengage sons.");
     alert("Shoot. Actually I have two teenage sons.");
@@ -47,6 +51,7 @@ let inCF = prompt ("Am I in a Code Fellows course?").toLowerCase();
 if (inCF === "yes") {
     //console.log("That's correct!")
     alert("That's correct!");
+    userScore++;
 } else {
     //console.log("Nope, actually I am.")
     alert("Nope, actually I am.");
@@ -57,6 +62,7 @@ let learnCybSec = prompt("Do I want to learn cyber security at CF?").toLowerCase
 if (learnCybSec === "no") {
     //console.log("That's correct!")
     alert("That's correct!");
+    userScore++;
 } else {
     //console.log("Close, but I'm learning software development, not cyber security.")
     alert("Close, but I'm learning software development, not cyber security.");
@@ -64,8 +70,79 @@ if (learnCybSec === "no") {
 
 
 
+//Lab Class 03
+
+
+let numCountries = 22; 
+let numGuesses = 4;
+
+
+/*
+
+let userGuess = prompt("Finally, guess how many different countries I've coached entrepreneurs from.").Number(userGuess);
+
+if (userGuess === numCountries) {
+    
+    alert("That's correct!")
+  }  else {
+        alert("Nope.")
+}
+*/
+
+for (let i = 1; i <= numGuesses; i++) {
+    let userGuessNum = prompt("Guess how many different countries I've coached entrepreneurs from.")
+    userGuessNum = Number(userGuessNum);
+    
+        if (userGuessNum === numCountries) {
+        alert("That's correct!");
+        userScore++;
+        break;
+
+    }   else {
+        alert("Nope.");
+    }
+}
+
+
+let favC = ["Germany", "Ireland", "Barbados", "South Africa", "France", "Vietnam", "Scotland", "Turkey", "Jordan", "Spain"];
+let numGuessesFavC = 6;
+let guess = false;
+
+
+
+for (let i = 1; i <= numGuessesFavC; i++){
+    let userGuessFavC = prompt("Guess one of my Top 10 favorite countries that I've traveled to and worked in.").normalize()
+
+
+    if (favC.includes(userGuessFavC)) {
+            alert("Yes, you got one of them! All ten are Germany, Ireland, Barbados, South Africa, France, Vietnam, Scotland, Turkey, Jordan and Spain.");
+            guess = true;
+            userScore++;
+            alert("Thanks for playing 'About me' trivia! Your score is " + userScore + ".")
+            break;
+
+    }   else {
+            alert("No, that's not one of them.")
+    }
+
+}
+
+    if (!guess) {
+        alert("Thanks for playing, but you're now out of guesses. All ten are Germany, Ireland, Barbados, South Africa, France, Vietnam, Scotland, Turkey, Jordan and Spain.")
+        alert("Thanks for playing 'About me' trivia! Your score is " + userScore + ".")
+    }
+
+
+    
+/*
+for (let i = 0; i <= numGuessesFavC.length; i++){
+    alert("All ten are Germany, Ireland, Barbados, South Africa, France, Vietnam, Scotland, Turkey, Jordan and Spain.")
+}
+*/
+
 
 //2.
 const userName = prompt("Great. That's it for the trivia on me. Now, what's your name?");
 alert("Welcome to my site, " + userName + ".");
 document.write("All that hard work on the prompts paid off, " + userName + ". Here my About Me page.");
+
